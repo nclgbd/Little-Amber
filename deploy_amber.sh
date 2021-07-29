842825f0b5ba872b21538ff2c5938bc840565f9b
nguobadia <nicoleguob@gmail.com>
2021-07-28 19:31:05 -0700

    Added workflow

---
+++ deploy_amber.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+ssh -i "config/.ssh/ec2tutkey.pem" ec2-user@ec2-34-216-70-159.us-west-2.compute.amazonaws.com "cd Little-Amber/ && forever stopall && git commit -am \"Syncing Library\" && git pull origin master && forever start -c python3 scripts/main.py"
