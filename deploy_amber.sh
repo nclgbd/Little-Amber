95140e3d6be1472d8f8690374b01781009767861
nguobadia <nicoleguob@gmail.com>
2021-07-29 19:38:29 -0700

    Modified launch-bot.yml

--- deploy_amber.sh
+++ deploy_amber.sh
@@ -3,6 +3,6 @@ forever stopall || return 5
 git add . || return 6
 message=$(git status) || return 7
 git commit -m "${message}" || return 8
-git pull || return 9
-git push || return 10
+git pull
+git push
 forever start -c python3 scripts/main.py || return 11
